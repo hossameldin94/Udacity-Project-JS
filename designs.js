881cf1a06ef8a0f20da6ad28934c313e78674d75
hyoussef <hossameldin.youssef@valeo.com>
2018-10-22 08:09:51 +0200

    changing the submit event of form tag & using the vscode formating for indentation

--- designs.js
+++ designs.js
@@ -1,40 +1,36 @@
-// Select color input
-// Select size input
-const btn = document.getElementById('submitBtn');
-btn.addEventListener('click',makeGrid);
-
-function dont(){
-	return false;
+function makeGrid(event) {
+	//prevent the default to avoid reloading the page
+	event.preventDefault();
+	// get the height value
+	var h = document.getElementById('inputHeight').valueAsNumber;
+	//get the width value
+	var w = document.getElementById('inputWidth').valueAsNumber;
+	// get the table element
+	const table = document.getElementById('pixelCanvas');
+	table.innerHTML = '';
+	for (var i = 0; i < h; i++) {
+		//create row
+		var row = document.createElement('tr');
+		for (var y = 0; y < w; y++) {
+			//create a colomn element
+			var cell = document.createElement('td');
+			//assign event color on click
+			cell.onclick = color;
+			//append to the row child cell
+			row.appendChild(cell);
+		}
+		//append the row after adding the colomns
+		table.appendChild(row);
+	}
+}
+//add event to call make grid when submit
+document.getElementById('sizePicker').addEventListener('submit', function (event) {
+	makeGrid(event);
+});
+//event color to change the color of cells
+function color() {
+	this.bgColor = document.getElementById('colorPicker').value;
 };
 
-document.getElementById('sizePicker').addEventListener('submit',function dont(e){
-	e.preventDefault();
-}
-);
 
-function color(){
-			this.bgColor = document.getElementById('colorPicker').value;
-		};
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-var h = document.getElementById('inputHeight').valueAsNumber;
-var w = document.getElementById('inputWidth').valueAsNumber;
-const table = document.getElementById('pixelCanvas');
-table.innerHTML = '';
-for (var i = 0 ; i< h ; i++){
-	var row = document.createElement('tr');
-	
-	for (var y =0 ; y< w;y++)
-	{
-		var cell = document.createElement('td');
-		cell.onclick = color;
-		//cell.addEventListener("onclick",
-		row.appendChild(cell);
-	}
-	table.appendChild(row);
-}
-}
 
